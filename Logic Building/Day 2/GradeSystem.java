b91a0dbf5f3669efcba80546bd88119dd51dbdd4
Aaditi Sarode <aaditi.sarode.cmaug26@gmail.com>
2026-08-21 17:39:23 +0530

    Add files via upload

---
+++ Logic Building/Day 2/GradeSystem.java
@@ -0,0 +1,28 @@
+public class GradeSystem{
+  public static void main(String[] args){
+    int maths = 80;
+    int science = 85;
+    int history = 90;
+ 
+   double average = (maths+ science+ history)/3.0;
+   System.out.println("Average marks: " + average);
+
+   if(average >= 90){
+    System.out.println("Grade: A");
+   }
+   else if(average >= 70){
+    System.out.println("Grade: B");
+  }
+   else if(average >= 50){
+    System.out.println("Grade: C");
+  }
+   else if(average >= 30){
+    System.out.println("Grade: D");
+  }
+   else{
+    System.out.println("Fail");
+  }
+
+ }
+}
+     
---
+++ Logic Building/Day 2/Variable.java
@@ -0,0 +1,25 @@
+public class Variable{
+ public static void main(String[] args){
+  
+
+   byte a = 20;
+   short b = 10;
+   int c = 5000;
+   long d = 40;
+   float e = 3.14f;
+   double f = 70.5;
+   char g = 'A';
+   Boolean h = true;
+
+  System.out.println("byte: " + a);
+  System.out.println("short: " + b);
+  System.out.println("int: " + c);
+  System.out.println("long: " + d);
+  System.out.println("float: " + e);
+  System.out.println("double: " + f);
+  System.out.println("char: " + g);
+  System.out.println("boolean: " + h);
+ }
+}
+
+  
